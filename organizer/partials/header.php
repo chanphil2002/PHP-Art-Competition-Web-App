fca9a6f204e9ef02853f5719343d0d3fea17d4e3
chanphil2002 <chanphil2002@gmail.com>
2022-05-21 05:24:21 +0800

    viewcomp ver1

--- organizer/partials/header.php
+++ organizer/partials/header.php
@@ -1,12 +1,13 @@
-<?php include ("../organizer/partials/database.php");?>
+<?php include("../organizer/partials/database.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Virtual-X</title>
-  
+
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <!-- Font Awesome -->
@@ -19,9 +20,10 @@
   <link rel="stylesheet" href="../organizer/partials/header.css">
 
 </head>
+
 <body>
   <nav class="navbar navbar-dark bg-dark bg-opacity-50 bg-gradient fixed-top px-5">
-    
+
     <a class="navbar-brand ml-5" href="    ">
       <img src="../organizer/partials/logo.png" alt="Virtual" width="200">
     </a>
@@ -31,7 +33,7 @@
     </form>
     <nav class="navbar navbar-expand-xl">
       <ul class="navbar-nav">
-        
+
         <li class="nav-item">
           <a class="btn btn-info" href="     " role="button"><i class="fa-solid fa-trophy"></i>&nbsp; My Competition</a>&nbsp;&nbsp;&nbsp;
         </li>
@@ -44,14 +46,16 @@
             <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
               <li><a class="dropdown-item" href="#">My Profile</a></li>
               <li><a class="dropdown-item" href="#">Favourite</a></li>
-              <li><hr class="dropdown-divider"></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
               <li><a class="dropdown-item text-danger" href="#">Log Out</a></li>
             </ul>
           </div>
         </li>
 
 
-        
+
 
 
 
@@ -61,42 +65,34 @@
       </ul>
     </nav>
   </nav>
-  
-<div class= "mt-5 mb-5 pt-5" ></div>
 
-<a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-  Link with href
-</a>
-<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-  Button with data-bs-target
-</button>
+  <div class="mt-5 mb-5 pt-5"></div>
 
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    <div>
-      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+  <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
-    <div class="dropdown mt-3">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-        Dropdown button
-      </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
+    <div class="offcanvas-body">
+      <div>
+        Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+      </div>
+      <div class="dropdown mt-3">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+          Dropdown button
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+      </div>
+
+
+
+
+
+
+
     </div>
-
-
-    
-
-
-
-
   </div>
-</div>
-
---
+++ organizer/viewcomp.php
@@ -0,0 +1,109 @@
+<?php include("../organizer/partials/header.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <img src="../materials/image/test1.jpg" class="img-fluid" alt="Responsive image">
+    <ul class="nav nav-tabs justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Main</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Announcement</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Scoring Rubric</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled">About</a>
+        </li>
+    </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <h1>Virtual Competition Arts 2022<span class="badge badge-success">Ongoing</span></h1>
+                <h3>By Sunway, ARTS</h3>
+
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Competition Date</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Scoring Format</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Prize Pool</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h2>
+                        Description
+                    </h2>
+                    <ul>
+                        <li>No cheating.</li>
+                        <li>Only NFT Related Content.</li>
+                        <li>All the best.</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col">
+                <ul>
+                    <li>No cheating.</li>
+                    <li>Only NFT Related Content.</li>
+                    <li>All the best.</li>
+                </ul>
+                <hr>
+                <h2><u>Winner</u></h2>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="justify-content-center">
+        <button type="button" class="btn btn-primary">Edit</button>
+        <button type="button" class="btn btn-danger">Delete</button>
+        <button type="button" class="btn btn-info">View Entries</button>
+        <button type="button" class="btn btn-info">View Feedback</button>
+    </div>
+</body>
+
+</html>
+
+<?php include("../organizer/partials/footer.php"); ?>
