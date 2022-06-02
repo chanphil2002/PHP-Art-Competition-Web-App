6cb9882c98dde838341e2a7070d5258680c4fbb4
Dllmmimi <mrnobodyjk@gmail.com>
2022-06-02 13:25:21 +0800

    gerg

--- organizer/partials/footer.php
+++ organizer/partials/footer.php
@@ -1,45 +1,56 @@
 <footer id="footer" class="pt-4 md-5 pt-md-5 border-top bg-dark bg-opacity-75 bg-gradient px-5 pb-3">
-  <div class="row">
-    <div class="col-6 col-md">
-      <h5 class="text-white">Virtual-X</h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-info" href="../aboutUs/aboutUs.php">About Us</a></li>
-        <li><a class="text-info" href="../product/shipping.php">Contact Us</a></li>
-        <li><a class="text-info" href="../product/returnandrefund.php">Privacy Policy</a></li>
-      </ul>
-    </div>
-    <div class="col-6 col-md">
-      <h5>Terms and Conditions</h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-info" href="../profile/profile.php">User</a></li>
-        <li><a class="text-info" href="../viewOrder/toShip.php">Organizer</a></li>
-      </ul>
-    </div>
-    <div class="col-6 col-md">
-      <h5>My Account</h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-info" href="../faq/faq.php">Account Setting</a></li>
-        <li><a class="text-info" href="../contactUs/contactUs.php">Report</a></li>
-      </ul>
-    </div>
-    <div class="col-6 col-md">
-      <h5>Follow Us On</h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-info" href="https://www.facebook.com/Organic-X-103220925584579"> <button type="button" class="btn btn-outline-primary">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fab fa-facebook"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button> </a></li> <br>
-        <li><a class="text-info" href="https://www.instagram.com/organicx123/"> <button type="button" class="btn btn-outline-danger">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fab fa-instagram"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button> </a></li>
-      </ul>
-    </div>
+    <div class="row">
+        <div class="col-6 col-md">
+            <h5 class="text-white">Virtual-X</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-info" href="../aboutUs/aboutUs.php">About Us</a></li>
+                <li><a class="text-info" href="../product/shipping.php">Contact Us</a></li>
+                <li><a class="text-info" href="../product/returnandrefund.php">Privacy Policy</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Terms and Conditions</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-info" href="../profile/profile.php">User</a></li>
+                <li><a class="text-info" href="../viewOrder/toShip.php">Organizer</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <h5>My Account</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-info" href="../faq/faq.php">Account Setting</a></li>
+                <li><a class="text-info" href="../contactUs/contactUs.php">Contact Admin</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Follow Us On</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-info" href="https://www.facebook.com/Organic-X-103220925584579"> <button
+                            type="button"
+                            class="btn btn-outline-primary">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i
+                                class="fab fa-facebook"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+                    </a></li> <br>
+                <li><a class="text-info" href="https://www.instagram.com/organicx123/"> <button type="button"
+                            class="btn btn-outline-danger">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i
+                                class="fab fa-instagram"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+                    </a></li>
+            </ul>
+        </div>
 
-  </div>
-  <small class="d-block mb-3 text-white text-center">Copyright &copy; 2022 Virtual-X. All Rights Reserved.</small>
-  <div class="col-12 col-md ">
-    <ul class="list-unstyled text-small text-center">
-      <li><a class="text-light" href="../product/term.php">Terms and Conditions &nbsp;</a> <a class="font-weight-normal text-muted">|&nbsp;</a> <a class="text-light" href="../product/privacy.php">Privacy Policy</a></li>
-    </ul>
-  </div>
+    </div>
+    <small class="d-block mb-3 text-white text-center">Copyright &copy; 2022 Virtual-X. All Rights Reserved.</small>
+    <div class="col-12 col-md ">
+        <ul class="list-unstyled text-small text-center">
+            <li><a class="text-light" href="../product/term.php">Terms and Conditions &nbsp;</a> <a
+                    class="font-weight-normal text-muted">|&nbsp;</a> <a class="text-light"
+                    href="../product/privacy.php">Privacy Policy</a></li>
+        </ul>
+    </div>
 </footer>
 <!-- jQuery and Bootstrap Bundle -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+</script>
 </body>
 
 </html>
