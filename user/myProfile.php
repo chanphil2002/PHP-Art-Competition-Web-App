aa603ea1a9d29bd481c32d7cf061d3045c5ba1a3
Jshan02 <jyeshan3770@gmail.com>
2022-06-02 21:26:31 +0800

    5percent profile

--- user/myProfile.php
+++ user/myProfile.php
@@ -1,12 +1,28 @@
+<?php
+    include ("partials/header.php");
+    include ("partials/database.php");
+
+    session_start();
+    if (!isset($_SESSION["user"])){
+        header("Location: ../general/registeredUserLogin.php");
+    }else{
+        $sql = "SELECT * FROM user WHERE userEmail = '$_SESSION[user]' ";
+        $res = mysqli_query($conn, $sql);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Profile</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../admin/addJudge.css" />
 </head>
-<body>
+<body class="form-v7">
     
 </body>
 </html>
